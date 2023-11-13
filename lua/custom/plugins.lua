030637e1c6903b2cb8ad9121189fd7740c0c1210
Zaar Hai <haizaar@haizaar.com>
2023-11-13 20:47:20 +1100

    Added markdown preview, though it does not work on Nix :D

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -64,6 +64,13 @@ local plugins = {
     end,
   },
 
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  }
+
 
   -- To make a plugin not be loaded
   -- {
