39962890238412cad558aad13c825c24471be99f
Zaar Hai <haizaar@haizaar.com>
2023-11-14 19:52:10 +1100

    Captial Y consistency makes more sense for me. As well set other mappings to easy rapid copy paste

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -7,9 +7,14 @@ M.general = {
     ["<A-t>"] = { "<cmd> NvimTreeToggle <CR>", "Toggle nvim-tree" },
     ["<A-f>"] = { "<cmd> NvimTreeFocus <CR>", "Focus nvim-tree" },
     ["<F5>"] = { "<cmd> Telescope undo<CR>", "Show undo history" },
+    ["<Y>"] = { "y$", "Yank from the cursor to the end of line" },
+    ["<A-p>"] = { '"zp', "Paste from the register z" },
+    ["<A-d>"] = { '"zd$', "Delete the line from cursor to the register z" },
   },
   v = {
     [">"] = { ">gv", "indent"},
+    ["<A-y>"] = { '"zy', "Yank into the register z"},
+    ["<A-d>"] = { '"zd', "Delete into the register z"},
   },
   i = {
     ["<F5>"] = { "<cmd> Telescope undo<CR>", "Show undo history" },
